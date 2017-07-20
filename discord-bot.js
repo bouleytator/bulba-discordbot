4d9bb18bde27b2e78c05989ec55a6cfc600ce483
Bulba <bouleytator@gmail.com>
2017-07-20 19:38:56 +0800

    Added bump command! | Removed FS

--- discord-bot.js
+++ discord-bot.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const { RichEmbed } = require('discord.js');
 const embed = new Discord.RichEmbed()
 const client = new Discord.Client();
-const fs = require("fs");
 const config = require("./settings.json");
 
 
