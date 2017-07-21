6dd35800a25c5a56c44f2bef986f707b91f4a1ec
Bulba <bouleytator@gmail.com>
2017-07-21 10:25:36 +0800

    Update installdep.bat

--- installdep.bat
+++ installdep.bat
@@ -1,8 +1,3 @@
-@echo off
+@echo on
 npm install discord.js --save
-echo.
-echo.
-echo Successfully installed all dependecies!
-echo Please don't touch folder "node-modules".
-echo It is the folder where dependecies are installed.
 pause >nul
